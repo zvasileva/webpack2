9b02d0b168c36d1058f0538ee3373babf4a9320f
zvasileva <zojka-homka@rambler.ru>
2020-05-30 01:00:56 +0300

    !!!

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
@@ -5,6 +7,28 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'app.bundle.js',
-	}
+	},
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+		  MiniCssExtractPlugin.loader, 'css-loader',
+		],
+      },
+    ],
+  },	
+	
+	plugins: [
+	  new HtmlWebpackPlugin({
+        hash: true,
+        template: './src/index.html',
+        filename: './index.html'
+	  }),
+      new MiniCssExtractPlugin({
+		filename:'style.css'
+      }),	  
+	]
+	
 }
 	
